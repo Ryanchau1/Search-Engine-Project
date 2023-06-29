a08c808d07bed759169848803915fc7117774c19
Ryan Chau <126630468+Ryanchau1@users.noreply.github.com>
2023-06-29 13:35:53 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const accessKey = "b7P7BKzc566aH6zKYuuPPsWjstfI7ka1U9hJlmV8ja0";
+  
+    const searchForm = document.getElementById("search-form");
+    const searchBox = document.getElementById("search-box");
+    const searchResult = document.getElementById("search-result");
+    const showMoreBtn = document.getElementById("show-more-btn");
+  
+    let keyword = "";
+    let page = 1;
+  
+    async function searchImages() {
+      keyword = searchBox.value;
+      const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`;
+      
+      const response = await fetch(url);
+      const data = await response.json();
+  
+      const results = data.results;
+  
+      results.forEach((result) => {
+        const img = document.createElement("img");
+        img.src = result.urls.small;
+        const imageLink = document.createElement("a");
+        imageLink.href = result.links.html;
+        imageLink.target = "_blank";
+        
+        imageLink.appendChild(img);
+        searchResult.appendChild(imageLink);
+      });
+      showMoreBtn.style.display = "block";
+    }
+  
+    searchForm.addEventListener("submit", async (e) => {
+      e.preventDefault();
+      page = 1;
+      await searchImages();
+    });
+
+    showMoreBtn.addEventListener("click", () => {
+        page++
+        searchImages();
+    })
+  });
